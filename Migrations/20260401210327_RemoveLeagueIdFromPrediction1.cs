3d3a1ec9d629afc0e016957d35f8dc16289f23d0
Konstantin Karamanov <konstantinkaramanov986@gmail.com>
2026-04-02 00:03:56 +0300

    Remove LeagueId from Prediction1

---
+++ Migrations/20260401210327_RemoveLeagueIdFromPrediction1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BPFL.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveLeagueIdFromPrediction1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
